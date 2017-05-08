f6e81d82c314672b612b36c03c523424271f4dd7
willyaugusto <willyaugusto@gmail.com>
2017-05-07 22:03:48 -0300

    geraçao de edmx

--- RedBus-api/Models/Mensagem.cs
+++ RedBus-api/Models/Mensagem.cs
@@ -22,7 +22,7 @@ namespace RedBus_api.Models
         public bool entregue { get; set; }
         public bool visualizada { get; set; }
     
-        public virtual Usuario UsuarioDe { get; set; }
-        public virtual Usuario UsuarioPara { get; set; }
+        public virtual Usuario Usuario { get; set; }
+        public virtual Usuario Usuario1 { get; set; }
     }
 }
